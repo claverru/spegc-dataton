a6dc73997fe0e57f378db9fe11b4753a3452d1f9
claverru <claverru@gmail.com>
2021-09-16 19:31:01 +0200

    added kfold

--- scripts/train_classifier.py
+++ scripts/train_classifier.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 
 import pytorch_lightning as pl
+from sklearn.model_selection import StratifiedKFold
 
 from src.model import Model
 
@@ -15,9 +16,9 @@ def get_parser():
     parser.add_argument('--accumulate-grad-batches', default=8, type=int)
     parser.add_argument('--batch-size', default=8, type=int)
     parser.add_argument('--img-size', default=512, type=int)
-    parser.add_argument('--backbone', default='mobilenetv3_large_100_miil')
+    parser.add_argument('--arch', default='mobilenetv3_large_100_miil')
 
-    parser.add_argument('--folds', nargs='+', default=[0, 1, 2, 3, 4])
+    parser.add_argument('--folds-to-train', nargs='+', default=[0, 1, 2, 3, 4])
     parser.add_argument('--kfolds', default=5, type=int)
     parser.add_argument('--kfold-seed', default=0, type=int)
 
@@ -37,10 +38,18 @@ if __name__ == '__main__':
     args = get_parser().parse_args()
 
     mode = 'min' if 'loss' in args.monitor else 'max'
-    assert args.kfolds >= len(args.folds)
+    assert args.kfolds >= len(args.folds_to_train)
 
-    # TODO: Add kfold
-    for i, (x, y) in range(10):
+    X = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    y = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
+
+    sfk = StratifiedKFold(n_splits=args.kfolds, shuffle=True, random_state=args.kfold_seed)
+    for i, (train_index, test_index) in enumerate(sfk.split(X, y)):
+
+        print(f'\nTraining {i+1}/{args.kfolds} fold')
+        if i not in args.folds_to_train:
+            print('Passing')
+            continue
 
         model = Model(
             arch='mobilenetv3_large_100_miil',
@@ -53,7 +62,8 @@ if __name__ == '__main__':
             pretrained=not args.from_scratch,
 
             # to be saved in hparams
-            img_size=args.img_size
+            img_size=args.img_size,
+            fold=i+1
         )
 
         trainer = pl.Trainer(
@@ -62,13 +72,13 @@ if __name__ == '__main__':
             benchmark=True,
             precision=16,
             callbacks=[
-                # pl.callbacks.ProgressBar(),
-                pl.callbacks.EarlyStopping(monitor=args.monitor, patience=args.patience, mode=mode),
+                pl.callbacks.ProgressBar(),
+                pl.callbacks.EarlyStopping(monitor=args.monitor, patience=args.lr_patience, mode=mode),
                 pl.callbacks.ModelCheckpoint(
                     monitor=args.monitor,
                     mode=mode,
-                    filename=f'{args.arch}_fold_{args.fold}_of_{args.nfolds}_' + '{epoch}_{args.monitor:.3f}'),
+                    filename=f'{args.arch}_fold_{i+1}_of_{args.kfolds}_' + '{epoch}_{args.monitor:.3f}'),
             ],
         )
 
-        print(model)
+        print(model.hparams)
